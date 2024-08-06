bd343f351890fd896897292bb9bed9ad96ce48d0
qualzed <125376893+qqqllllzzzz@users.noreply.github.com>
2024-08-06 18:42:33 +0300

    Delete Scripts/texts.hpp

--- Scripts/texts.hpp
+++
@@ -1,9 +0,0 @@
-#include "colors.hpp"
-#include <string>
-
-using namespace std;
-
-void logo() {
-    cout << br_red << "Scr" << green << "ea" << br_blue << "mH" << magenta << "a" << yellow << "ck";
-    cout << "\nType 'help' to get a list of commands.\n";
-}
